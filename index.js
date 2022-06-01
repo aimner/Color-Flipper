881f64c5e4ff518f8ca8e4ee7a9d8fdf61d7b325
aimner <aimner510@gmail.com>
2022-06-01 15:45:43 +0300

    feat: start project

---
+++ index.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('.container')
+const changeColor = document.querySelector('.change_color')
+const colorTitle = document.querySelector('.color_title')
+
+
+
+
+
+
+
+changeColor.addEventListener('click', () => {
+    let firstNumber1 = Math.floor(Math.random() * 250)
+    let secondNumber1 = Math.floor(Math.random() * 250)
+    let thirdNumber1 = Math.floor(Math.random() * 250)
+
+    let firstNumber2 = Math.floor(Math.random() * 250)
+    let secondNumber2 = Math.floor(Math.random() * 250)
+    let thirdNumber2 = Math.floor(Math.random() * 250)
+
+    let firstNumber3 = Math.floor(Math.random() * 250)
+    let secondNumber3 = Math.floor(Math.random() * 250)
+    let thirdNumber3 = Math.floor(Math.random() * 250)
+
+    let firstProcent = Math.floor(Math.random() * 100)
+    let secondProcent = Math.floor(Math.random() * 100)
+    let thirdProcent = Math.floor(Math.random() * 100)
+
+    let degree = Math.floor(Math.random() * 360)
+
+    container.style.background = `linear-gradient(${degree}deg, rgba(${firstNumber1},${secondNumber1},${thirdNumber1}) ${firstProcent}%, rgba(${firstNumber2},${secondNumber2},${thirdNumber2}) ${secondProcent}%,
+    rgba(${firstNumber3},${secondNumber3},${thirdNumber3}) ${thirdProcent}%)`
+    colorTitle.textContent = `linear-gradient (${degree}deg, rgba(${firstNumber1},${secondNumber1},${thirdNumber1}) ${firstProcent}%, rgba(${firstNumber2},${secondNumber2},${thirdNumber2}) ${secondProcent}%,
+    rgba(${firstNumber3},${secondNumber3},${thirdNumber3}) ${thirdProcent}%)`
+    
+})
+
+
+
