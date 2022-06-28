302ef7aabd4a308f42e588a5c533c4e58e3b7614
aimner <aimner510@gmail.com>
2022-06-28 18:24:59 +0300

    feat: create function to change data number after delete procent scroll button

--- modules/changeProcentFunctionScroll.js
+++ modules/changeProcentFunctionScroll.js
@@ -10,7 +10,7 @@ function scrollProcentPanel(item, inputsArr) {
         elem.active = false;
         if (elem.dataNumber == item.dataset.number) {
             elem.active = true;
-            chooseFunctionToChangeButtonsElement (elem)     
+            chooseFunctionToChangeButtonsElement(elem)
         }
     })
 
@@ -26,7 +26,7 @@ function scrollProcentPanel(item, inputsArr) {
 }
 
 
-function chooseFunctionToChangeButtonsElement (elem) {
+function chooseFunctionToChangeButtonsElement(elem) {
     switch (elem.controlPointGaps) {
         case 1:
             chooseFunctions2.choose1(colorPanel, elem)
@@ -61,14 +61,19 @@ function changeProcentScroll(event, item, inputsArr) {
     let mouseСoordinatesProcentPanel = event.clientX - procentScroll.offsetLeft;
     item.style.left = `${mouseСoordinatesProcentPanel - (procentScrollControlButton.offsetWidth / 2)}px`;
     inputsArr.forEach(input => {
+
         if (item.dataset.number === input.dataset.number) {
+
             input.style.left = `${mouseСoordinatesProcentPanel - (item.offsetWidth / 2) - (procentScrollText.offsetWidth / 2 - item.offsetWidth / 2)}px`;
             let transparency = (+item.style.left.split('').filter(item => !isNaN(+item) || item === '.').join('')).toFixed(0);
             input.value = (transparency * 100 / procentScroll.offsetWidth).toFixed(0);
             let elem = globalButtonsArr.find(item => item.active === true);
+
             elem.procent = input.value;
+
             colorTitle.style.background = elem.color();
             procentScroll.style.background = elem.colorProcentScroll();
+
         }
     })
 }
@@ -77,51 +82,90 @@ function changeProcentScroll(event, item, inputsArr) {
 
 
 function changeButtonsElement(event) {
-    if(event.target.classList.contains('colors_block_choose_button')) {
+    if (event.target.classList.contains('colors_block_choose_button')) {
         procentScrollButtonsArr.forEach(item => item.classList.remove('procent_scroll_control_button_active'));
         procentScrollButtonsArr.find(item => item.dataset.number === event.target.dataset.number).classList.add('procent_scroll_control_button_active');
         globalButtonsArr.forEach(item => {
             item.active = false
-            if(item.dataNumber === +event.target.dataset.number) {
+            if (item.dataNumber === +event.target.dataset.number) {
                 item.active = true
                 chooseFunctionToChangeButtonsElement(item)
             }
         });
-       }
+    }
 
-    if(event.target.classList.contains('colors_block_delete_button')) {
-       
-       let deleteButtonControl = procentScrollButtonsArr.find(item => item.dataset.number === event.target.dataset.number);
-       deleteButtonControl.remove();
-       procentScrollButtonsArr.splice(procentScrollButtonsArr.indexOf(deleteButtonControl), procentScrollButtonsArr.indexOf(deleteButtonControl))
+    if (event.target.classList.contains('colors_block_delete_button')) {
 
-       let deleteInput = procentScrollInputsArr.find(item => item.dataset.number === event.target.dataset.number);
-       deleteInput.remove()
-       procentScrollInputsArr.splice(procentScrollInputsArr.indexOf(deleteInput), procentScrollInputsArr.indexOf(deleteInput))
+        let deleteButtonControl = procentScrollButtonsArr.find(item => item.dataset.number === event.target.dataset.number);
+        for (let i = 1 + procentScrollButtonsArr.indexOf(deleteButtonControl); i < procentScrollButtonsArr.length; i++) {
+            procentScrollButtonsArr[i].dataset.number = +procentScrollButtonsArr[i].dataset.number - 1;
+            procentScrollButtonsArr[i].textContent = +procentScrollButtonsArr[i].textContent - 1;
+            procentScrollButtonsArr[i].id = `procent_scroll_control_button_${i}`;
+        }
+        procentScrollButtonsArr.splice(procentScrollButtonsArr.indexOf(deleteButtonControl), 1)
+        deleteButtonControl.remove();
 
-       let deleteChooseColorButton = globalButtonsArr.indexOf(globalButtonsArr.find(item => item.dataNumber === +event.target.dataset.number));
-       globalButtonsArr.splice(deleteChooseColorButton, deleteChooseColorButton)
+/////////////////////////////////////////
 
-       let delete1 = colorsBlockChooseButtonsArr.find(item => item.dataset.number === event.target.dataset.number);
-       colorsBlockChooseButtonsArr.splice(colorsBlockChooseButtonsArr.indexOf(delete1), colorsBlockChooseButtonsArr.indexOf(delete1))
-    
+        let deleteInput = procentScrollInputsArr.find(item => item.dataset.number === event.target.dataset.number);
+        for (let i = 1 + procentScrollInputsArr.indexOf(deleteInput); i < procentScrollInputsArr.length; i++) {
+            procentScrollInputsArr[i].dataset.number = +procentScrollInputsArr[i].dataset.number - 1;
+            procentScrollInputsArr[i].textContent = +procentScrollInputsArr[i].textContent - 1;
+            procentScrollInputsArr[i].id = `procent_scroll_text_${i}`;
+        }
+        procentScrollInputsArr.splice(procentScrollInputsArr.indexOf(deleteInput), 1)
+        deleteInput.remove()
+/////////////////////////////////////////
+        let deleteGlobalButtonsArrElement = globalButtonsArr.indexOf(globalButtonsArr.find(item => item.dataNumber === +event.target.dataset.number));
+        for (let i = 1 + deleteGlobalButtonsArrElement; i < globalButtonsArr.length; i++) {
+            globalButtonsArr[i].dataNumber -= 1;
+        }
+        globalButtonsArr.splice(deleteGlobalButtonsArrElement, 1)
 
-       let delete2 = colorsBlockDeleteButtonsArr.find(item => item.dataset.number === event.target.dataset.number);
-       colorsBlockDeleteButtonsArr.splice(colorsBlockDeleteButtonsArr.indexOf(delete2), colorsBlockDeleteButtonsArr.indexOf(delete2))
+/////////////////////////////////////////
+        let deleteChooseButton = colorsBlockChooseButtonsArr.find(item => item.dataset.number === event.target.dataset.number);
+        for (let i = 1 + colorsBlockChooseButtonsArr.indexOf(deleteChooseButton); i < colorsBlockChooseButtonsArr.length; i++) {
+            colorsBlockChooseButtonsArr[i].dataset.number = +colorsBlockChooseButtonsArr[i].dataset.number - 1;
+            colorsBlockChooseButtonsArr[i].textContent = `Choose ${i} Color`;
+        }
+        colorsBlockChooseButtonsArr.splice(colorsBlockChooseButtonsArr.indexOf(deleteChooseButton), 1)
+/////////////////////////////////////////
+        let deleteDeleteButton = colorsBlockDeleteButtonsArr.find(item => item.dataset.number === event.target.dataset.number);
+        for (let i = 1 + colorsBlockDeleteButtonsArr.indexOf(deleteDeleteButton); i < colorsBlockDeleteButtonsArr.length; i++) {
+            colorsBlockDeleteButtonsArr[i].dataset.number = +colorsBlockDeleteButtonsArr[i].dataset.number - 1;
+            colorsBlockDeleteButtonsArr[i].textContent = `Delete ${i} Color`;
+        }
+        colorsBlockDeleteButtonsArr.splice(colorsBlockDeleteButtonsArr.indexOf(deleteDeleteButton), 1)
+/////////////////////////////////////////
+        let deleteExmapleColor = colorsExampleArr.find(item => item.dataset.number === event.target.dataset.number);
+        for (let i = 1 + colorsExampleArr.indexOf(deleteExmapleColor); i < colorsExampleArr.length; i++) {
+            colorsExampleArr[i].dataset.number = +colorsExampleArr[i].dataset.number - 1;
+        }
+        colorsExampleArr.splice(colorsExampleArr.indexOf(deleteExmapleColor), 1)
 
-       event.target.parentElement.remove()
+////////////////////////////////////////
+        event.target.parentElement.remove()
 
-       colorTitle.style.background = globalButtonsArr[0].color();
+        colorTitle.style.background = globalButtonsArr[0].color();
+/////////////////////////////////////////
+        console.log(globalButtonsArr)
+        console.log(procentScrollButtonsArr)
+        console.log(procentScrollInputsArr)
+        console.log(colorsBlockChooseButtonsArr)
+        console.log(colorsBlockDeleteButtonsArr)
+        console.log(colorsExampleArr)
 
-       console.log(globalButtonsArr)
-       console.log(procentScrollButtonsArr)
-       console.log(procentScrollInputsArr)
-       console.log(colorsBlockChooseButtonsArr)
-       console.log(colorsBlockDeleteButtonsArr)
-   
-       
+
     }
 }
+
+// function changeNumber(arr, deleteElem) {
+//     for (let i = 1 + arr.indexOf(deleteElem); i < arr.length; i++) {
+//         arr[i].dataset.number = +arr[i].dataset.number - 1;
+//         arr[i].textContent = +arr[i].textContent - 1;
+//         arr[i].id = `procent_scroll_control_button_${i}`;
+//     }
+// }
 
 function createControlProcentButton(event) {
     let newButton = document.createElement('div');
@@ -134,13 +178,13 @@ function createControlProcentButton(event) {
     let mouseСoordinatesProcentPanel = event.clientX - procentScroll.offsetLeft;
     newButton.style.left = `${mouseСoordinatesProcentPanel - newButton.offsetWidth / 2}px`;
     procentScroll.append(newButton);
-    
+
     createControlProcentInput(newButton, mouseСoordinatesProcentPanel);
 
 }
 
 
-function createControlProcentInput (newButton, mouseСoordinatesProcentPanel) {
+function createControlProcentInput(newButton, mouseСoordinatesProcentPanel) {
     let newInput = document.createElement('input');
     procentScrollInputsArr.push(newInput);
     newInput.classList.add('procent_scroll_text');
